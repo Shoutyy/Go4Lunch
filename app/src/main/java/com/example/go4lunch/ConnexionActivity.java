c07780d630714dafa398d493c48e83c594368f13
Benjamin Debray <shouty91600@hotmail.fr>
2021-07-08 14:38:46 +0200

    update login page

--- app/src/main/java/com/example/go4lunch/ConnexionActivity.java
+++ app/src/main/java/com/example/go4lunch/ConnexionActivity.java
@@ -1,22 +1,63 @@
 package com.example.go4lunch;
 
 import android.os.Bundle;
-import android.view.Window;
 import android.view.WindowManager;
+
+import com.example.go4lunch.databinding.ActivityConnexionBinding;
+import com.firebase.ui.auth.AuthUI;
+
+import java.util.Collections;
+import java.util.List;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+
 public class ConnexionActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 123;
+    private ActivityConnexionBinding binding;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // hide actionBar
         if (getSupportActionBar() != null) {
             getSupportActionBar().hide();
         }
+
+        // hide notificationBar
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        binding = ActivityConnexionBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_connexion);
+        setupListeners();
     }
 
+    private void setupListeners(){
+        // mail button
+        binding.mailButton.setOnClickListener(view -> {
+            startSignInActivity();
+        });
+    }
+
+    private void startSignInActivity() {
+        // Choose authentication providers
+        List<AuthUI.IdpConfig> providers =
+                Collections.singletonList(new AuthUI.IdpConfig.EmailBuilder().build());
+
+        // Launch the activity
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setTheme(R.style.LoginTheme)
+                        .setAvailableProviders(providers)
+                        .setIsSmartLockEnabled(false, true)
+                        .setLogo(R.drawable.white_bowl_lowsize)
+                        .build(),
+                RC_SIGN_IN);
+    }
 }
+
+
