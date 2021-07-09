e2c1d2807792c9873bf351eb71d39e24d4acdb57
Benjamin Debray <shouty91600@hotmail.fr>
2021-07-09 16:59:25 +0200

    add twitter activity

--- app/src/main/java/com/example/go4lunch/ConnexionActivity.java
+++ app/src/main/java/com/example/go4lunch/ConnexionActivity.java
@@ -3,24 +3,34 @@ package com.example.go4lunch;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 import com.example.go4lunch.databinding.ActivityConnexionBinding;
 import com.firebase.ui.auth.AuthMethodPickerLayout;
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.ErrorCodes;
 import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.OAuthProvider;
+
 
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 
 public class ConnexionActivity extends BaseActivity<ActivityConnexionBinding> {
 
     private static final int RC_SIGN_IN = 123;
+    FirebaseAuth firebaseAuth;
 
 
     ActivityConnexionBinding getViewBinding() {
@@ -51,6 +61,7 @@ public class ConnexionActivity extends BaseActivity<ActivityConnexionBinding> {
             .Builder(R.layout.activity_authentication)
             .setGoogleButtonId(R.id.googleButton)
             .setEmailButtonId(R.id.mailButton)
+            .setTwitterButtonId(R.id.twitterButton)
             // ...
             //.setTosAndPrivacyPolicyId(R.id.baz)
             .build();
@@ -60,6 +71,7 @@ public class ConnexionActivity extends BaseActivity<ActivityConnexionBinding> {
         // Choose authentication providers
         List<AuthUI.IdpConfig> providers = Arrays.asList(
                 new AuthUI.IdpConfig.GoogleBuilder().build(),
+                new AuthUI.IdpConfig.TwitterBuilder().build(),
                 new AuthUI.IdpConfig.EmailBuilder().build());
 
         // Launch the activity
