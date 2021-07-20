77e32e3a95fa9b9c23c624f4caf37b51b31b0f4b
Benjamin Debray <shouty91600@hotmail.fr>
2021-07-20 16:51:31 +0200

    Place autocomplete

--- app/src/main/java/com/example/go4lunch/ui/map/MapFragment.java
+++ app/src/main/java/com/example/go4lunch/ui/map/MapFragment.java
@@ -8,6 +8,7 @@ import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,6 +21,7 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.go4lunch.R;
 import com.example.go4lunch.databinding.FragmentMapBinding;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -29,8 +31,18 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.Task;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.model.RectangularBounds;
+import com.google.android.libraries.places.api.model.TypeFilter;
+import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
+
 
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
 
 
 public class MapFragment extends Fragment implements OnMapReadyCallback {
@@ -40,25 +52,47 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     FusedLocationProviderClient client;
     SupportMapFragment supportMapFragment;
     GoogleMap mGoogleMap;
+    private static final String TAG = "MyMapFragment";
 
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+
         mapViewModel =
                 new ViewModelProvider(this).get(MapViewModel.class);
 
         binding = FragmentMapBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        /*
-        final TextView textView = binding.textHome;
-        mapViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        String apiKey = getString(R.string.api_key);
+        Places.initialize(getActivity().getApplicationContext(), apiKey);
+
+        PlacesClient placesClient = Places.createClient(getContext());
+
+        AutocompleteSupportFragment autocompleteFragment = (AutocompleteSupportFragment) getChildFragmentManager().findFragmentById(R.id.autocomplete_fragment);
+
+        autocompleteFragment.setTypeFilter(TypeFilter.ESTABLISHMENT);
+        autocompleteFragment.setCountries("FR");
+
+        autocompleteFragment.setLocationBias(RectangularBounds.newInstance(
+                new LatLng(48.8534, 2.3488),
+                new LatLng(48.8534, 2.3488)
+        ));
+
+
+        autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME, Place.Field.LAT_LNG));
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onPlaceSelected(@NonNull @NotNull Place place) {
+                Log.i(TAG ,"Place: " + place.getName() + ", " + place.getId());
             }
-        }); */
-        // checkMyPermission();
+
+            @Override
+            public void onError(@NonNull @NotNull Status status) {
+                Log.i(TAG , "An error occurred: " + status);
+            }
+        });
+
 
         supportMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
         supportMapFragment.getMapAsync(this);
@@ -72,31 +106,6 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
 
         return root;
     }
-/*
-    private void checkMyPermission() {
-        Dexter.withContext(getContext()).withPermission(Manifest.permission.ACCESS_FINE_LOCATION).withListener(new PermissionListener() {
-            @Override
-            public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
-                Toast.makeText(getContext(), "Permission granted", Toast.LENGTH_SHORT).show();
-
-            }
-
-            @Override
-            public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
-                Intent intent = new Intent();
-                intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
-                Uri uri = Uri.fromParts("package", getActivity().getPackageName(), "");
-                intent.setData(uri);
-                startActivity(intent);
-            }
-
-            @Override
-            public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
-                permissionToken.continuePermissionRequest();
-            }
-        }).check();
-    } */
-
 
     @Override
     public void onDestroyView() {
