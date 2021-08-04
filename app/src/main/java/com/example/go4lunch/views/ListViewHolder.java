e8a634be4b82a01665936a599d3da1ce7b68ddfd
Benjamin Debray <shouty91600@hotmail.fr>
2021-08-04 10:57:35 +0200

    layout list

---
+++ app/src/main/java/com/example/go4lunch/views/ListViewHolder.java
@@ -0,0 +1,13 @@
+package com.example.go4lunch.views;
+
+import android.view.View;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ListViewHolder extends RecyclerView.ViewHolder {
+
+    public ListViewHolder(View itemView) {
+
+        super(itemView);
+    }
+}
